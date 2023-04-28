2ca1ea7fd3a95c9afd9397ffde01ceb44740b9ea
Bram Borggreve <beeman@beeman.nl>
2023-04-28 10:49:01 +0100

    chore: add more links

--- source.ts
+++ source.ts
@@ -8,6 +8,11 @@ export const source: Category[] = [
       'https://staging.bsky.app/profile/haiku-bot.bsky.social',
       'https://github.com/ssempervirens/bluesky-haiku',
     ),
+    link(
+      'Bluesky Account Creator',
+      'https://bluesky-account-creator.vercel.app/',
+      'https://github.com/akiomik/bluesky-account-creator',
+    ),
     link('Bot to send hugs to other users on bluesky', 'https://github.com/haideralipunjabi/bluesky-hugfairy'),
     link(
       'Bot that posts earthquakes of 5.0+ magnitude',
@@ -19,11 +24,11 @@ export const source: Category[] = [
       'https://staging.bsky.app/profile/limerick.bot.gar.lol',
       'https://github.com/rothos/limerickbot',
     ),
-    link('Just follow people in Bluesky Social.', 'https://github.com/myConsciousness/bluesky-follow-bot'),
+    link('Just follow people in Bluesky Social', 'https://github.com/myConsciousness/bluesky-follow-bot'),
     link('Some bot programs for the bluesky network', 'https://github.com/whyrusleeping/bskybots'),
     link('@vvvot.bsky.social bot implementation', '@vvvot.bsky.social', 'https://github.com/vvakame/atproto-vvvot'), //
     link(
-      'Bot that posts food safety notices from the UK Food Standards Agency to your Bluesky feed.',
+      'Bot that posts food safety notices from the UK Food Standards Agency to your Bluesky feed',
       'https://github.com/lovelaced/bluesky-food-safety-bot',
     ),
     link('APOD Bot for Bluesky Social', 'https://github.com/myConsciousness/bluesky-apod-bot'),
@@ -53,7 +58,7 @@ export const source: Category[] = [
     links('Python', [
       link('Python async bluesky client and TUI (eventually)', 'https://github.com/jbn/psychonaut'),
       link(
-        'A nano implementation of the AT Protocol (Authenticated Transfer Protocol) for Python.',
+        'A nano implementation of the AT Protocol (Authenticated Transfer Protocol) for Python',
         'https://github.com/susumuota/nanoatp',
       ),
       link(
@@ -70,47 +75,68 @@ export const source: Category[] = [
       link('Dart/Flutter BlueSky client', 'https://github.com/tacsotai/flutter_bluesky.git'),
     ]),
     links('Go', [
+      link('Bluesky Go Library', 'https://github.com/PiccoloMondoC/bluesky'),
       link('CLI Application for Bluesky Social', 'https://github.com/mattn/bsky'),
       link('Simple golang firehose for Bluesky', 'https://github.com/CharlesDardaman/blueskyfirehose'),
     ]),
     links('Java', [
-      //
       link('Bluesky/ATProtocol client for java', 'https://github.com/uakihir0/bsky4j'),
-    ]),
-    links('Java', [
-      link('Android/desktop client for Bluesky', 'https://github.com/christiandeange/ozone'),
       link('SoftLibATP', 'https://github.com/okomeki/SoftLibATP'),
+    ]),
+    links('Kotlin', [
+      link(
+        'An experimental ATP/Bluesky client app for Android',
+        'https://play.google.com/store/apps/details?id=io.github.akiomik.seiun',
+        'https://github.com/akiomik/seiun',
+      ),
+      link('Android/desktop client for Bluesky', 'https://github.com/christiandeange/ozone'),
     ]),
     links('Rust', [
       link('Bluesky API library', 'https://github.com/jesopo/bisky'),
       link('The ATProto(Bluesky) api impl in Rust', 'https://github.com/sizumita/bsky-rs'),
+      link('A suite of libraries, tools, and daemons for atproto', 'https://github.com/ngerakines/atproto-rs'),
     ]),
     links('Swift', [
       //
       link('Unofficial Bluesky/ATProto client in SwiftUI', 'https://github.com/rmcan/swiftsky.git'),
     ]),
     links('TypeScript', [
-      link('Example React Native app for AT Protocol', 'https://github.com/pfrazee/example-atproto-rn-app'),
-      link('CLI tools for posting on bluesky', 'https://github.com/Jazzkid0/bsky-cli-tools'),
       link('Bluesky client for Mac, Linux and Windows', 'https://github.com/pram11/Jet'),
+      link('Bluesky Expo client', 'https://github.com/jonsamp/bluesky-expo'),
+      link('Bluesky React Native Example app for AT Protocol', 'https://github.com/pfrazee/example-atproto-rn-app'),
+      link('CLI tools for posting on bluesky', 'https://github.com/Jazzkid0/bsky-cli-tools'),
       link('Web client for Bluesky using Next.js', 'https://github.com/ajroberts0417/azure'),
       link('Web client for Bluesky using Next.js', 'https://github.com/KingYoSun/boobeeblue'),
       link('Web client for Bluesky using Svelte', 'https://github.com/tautologer/poastbox'),
+      link(
+        'Web client for Bluesky using Vue',
+        'https://penpenpng.github.io/skylight/',
+        'https://github.com/penpenpng/skylight',
+      ),
+      link('Web client for Bluesky using Vue', 'https://at.syui.ai/', 'https://github.com/syui/at.syui.ai'),
+      link('Web client for Bluesky', 'https://blueskyweb.vercel.app/', 'https://github.com/fredabila/blueskyweb'),
       link('Web client for Bluesky', 'https://flat-bs.vercel.app', 'https://github.com/sabigara/flat'),
-      link('Web client for Bluesky', 'https://treyp.github.io/nightsky', 'https://github.com/treyp/nightsky'),
       link('Web client for Bluesky', 'https://github.com/angelocho/BlueSkyweb'),
-      link('Bluesky web client using Vue', 'https://at.syui.ai/', 'https://github.com/syui/at.syui.ai'),
+      link('Web client for Bluesky', 'https://treyp.github.io/nightsky', 'https://github.com/treyp/nightsky'),
     ]),
   ]),
-  linkGroup('Demos and Starter Kits', [
+  linkGroup('Demos, Experiments and Starter Kits', [
     links('TypeScript', [
       link('ATProto Starter Kit', 'https://github.com/aliceisjustplaying/atproto-starter-kit'),
+      link('Bluesky Experiments', 'https://github.com/susumuota/bluesky-exp'),
       link('Bluesky Photo Test', 'https://github.com/robpc/bluesky-photo-test'),
       link('Practice Bluesky API (deno)', 'https://github.com/Neos21/practice-bsky'),
     ]),
   ]),
   links('Tools', [
-    // link('', '', ''),
+    link('', '', ''),
+    link(
+      'A GitHub Actions workflow to send a post to Bluesky',
+      'https://github.com/marketplace/actions/send-bluesky-post',
+      'https://github.com/myConsciousness/bluesky-post',
+    ),
+    link('Post new words used in my blog posts to Bluesky.', 'https://github.com/capjamesg/newwords'),
+    link('Japanese ATProto Docs', 'https://github.com/encrypteduse/atproto-website-docs-jp'),
     link('A GitHub Action to post to Bluesky Social', 'https://github.com/zentered/bluesky-post-action'),
     link(
       'Bluesky Follow Back All',
@@ -135,5 +161,9 @@ export const source: Category[] = [
       'https://github.com/jiftechnify/blue-mirage',
     ),
     link('UX improvements for Bluesky web app (browser extension)', 'https://github.com/xenohunter/bluesky-overhaul'),
+    link(
+      "NightSky is a browser extension that transforms your BlueSky social experience into a sleek, dark interface that's easy on the eyes",
+      'https://github.com/asamaree/NightSky',
+    ),
   ]),
 ]
